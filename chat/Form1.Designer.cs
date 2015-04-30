a46900b32978eef553cc330951945c1a5b5f7e06
Aaron Felix <aaron.felix@gmail.com>
2015-04-29 21:55:27 -0600

    initial commit

---
+++ chat/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 234);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(333, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(339, 74);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(62, 234);
+            this.listBox2.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening_1);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorsToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundColorsToolStripMenuItem,
+            this.buttonColorToolStripMenuItem,
+            this.textColorToolStripMenuItem});
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            // 
+            // backgroundColorsToolStripMenuItem
+            // 
+            this.backgroundColorsToolStripMenuItem.Name = "backgroundColorsToolStripMenuItem";
+            this.backgroundColorsToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.backgroundColorsToolStripMenuItem.Text = "Background Color";
+            this.backgroundColorsToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorsToolStripMenuItem_Click);
+            // 
+            // buttonColorToolStripMenuItem
+            // 
+            this.buttonColorToolStripMenuItem.Name = "buttonColorToolStripMenuItem";
+            this.buttonColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.buttonColorToolStripMenuItem.Text = "Button Color";
+            this.buttonColorToolStripMenuItem.Click += new System.EventHandler(this.buttonColorToolStripMenuItem_Click);
+            // 
+            // textColorToolStripMenuItem
+            // 
+            this.textColorToolStripMenuItem.Name = "textColorToolStripMenuItem";
+            this.textColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.textColorToolStripMenuItem.Text = "Text Color";
+            this.textColorToolStripMenuItem.Click += new System.EventHandler(this.textColorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(400, 315);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textColorToolStripMenuItem;
+    }
+}
+
---
+++ chat/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+namespace chat
+{
+    public partial class Form1 : Form
+
+    {
+        public string wordschecker = "";
+        public bool buttonpressed;
+        public string username = "";
+        public string password = "";
+        public string colorpickermode;
+        string backcolorhex = "#7796A9";
+        string buttoncolorhex = "#BBBBBB";
+        string buttonoutlinehex = "#BBBBBB";
+        string textcolorhex = "#DBEAF9";
+        Color backcolor;
+        Color buttoncolor;
+        Color buttonoutline;
+        Color textcolor;
+        string serverurl = null;
+        
+        public Form1()
+        
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("initializing");
+            listBox1.Items.Add("Please enter username");
+            textBox1.Select();
+
+            backcolor = System.Drawing.ColorTranslator.FromHtml(backcolorhex);
+            buttoncolor = System.Drawing.ColorTranslator.FromHtml(buttoncolorhex);
+            buttonoutline = System.Drawing.ColorTranslator.FromHtml(buttoncolorhex);
+            textcolor = System.Drawing.ColorTranslator.FromHtml(textcolorhex);
+
+            serverurl = System.IO.File.ReadAllText(@"serverurl.txt");
+
+
+
+
+
+            listBox1.BackColor = backcolor;
+            this.BackColor = backcolor;
+            listBox2.BackColor = backcolor;
+            button1.BackColor = buttoncolor;
+            button1.FlatAppearance.BorderColor = buttonoutline;
+            buttonpressed = false;
+          
+
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username == "" && textBox1.Text != "") {
+                username = textBox1.Text;
+                textBox1.Text = "";
+                Stage2_auth();
+            }
+            else if (password == "" && textBox1.Text != "")
+            {
+                password = textBox1.Text;
+                textBox1.Text = "";
+                DoChat();
+            }
+            else {
+                /*I think it doesn't work because of this block but I have no idea why*/
+                HttpWebRequest sendmessagerequest = (HttpWebRequest)WebRequest.Create(serverurl);
+                sendmessagerequest.KeepAlive = false;
+                sendmessagerequest.Method = "POST";
+                byte[] byteArray = Encoding.UTF8.GetBytes("message=" + textBox1.Text + "&user=" + username);
+                sendmessagerequest.ContentType = "chatMessageSubmit";
+                Stream dataStream = sendmessagerequest.GetRequestStream();
+                Console.WriteLine(sendmessagerequest.GetRequestStream());
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+                dataStream.Dispose();            
+                /*probably something doesn't go away after the loop, causing overflow*/
+            }
+            
+        }
+        private void Stage2_auth() {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Please enter password");
+        }
+
+        private void DoChat() {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("Connecting...");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverurl + "?user=" + username + "&pass=" + password);
+            
+            Timer timer = new Timer();
+            timer.Interval = 2000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            /* HttpWebRequest request = (HttpWebRequest)WebRequest.Create("serverurl");
+             request.Method = "GET";
+             Console.WriteLine("Request fulfilled:");
+            
+            
+             Console.WriteLine(request.GetResponse());*/
+            string s;
+            using (WebClient client = new WebClient())
+            {
+                s = client.DownloadString(serverurl);
+            }
+
+            if (s != wordschecker)
+            {
+
+                string[] words = s.Replace("\r\n", "\n").Replace("\n", " ").Replace("\r", " ").Replace("  ", " ").Split(" ".ToCharArray());
+                Console.WriteLine(words.Length);
+                Console.WriteLine(words[0]);
+                Console.WriteLine(words[1]);
+                //Console.WriteLine(words[90]);
+                words[0] = "first!";
+
+
+                listBox1.Items.Clear();
+                for (int i = 0; i < words.Length - (words.Length % 3); i = i + 3)
+                {
+                    if (words[i] != " ")
+                    {
+                        listBox1.Items.Add(words[i + 1] + ": " + words[i]);
+                    }
+
+
+
+                }
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 13){
+                button1_Click(null, null);
+            }
+           
+        }
+
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening_1(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void backgroundColorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorpickermode = "background";
+            colorDialog1.ShowDialog();
+            backcolor = colorDialog1.Color;
+            listBox1.BackColor = backcolor;
+            listBox2.BackColor = backcolor;
+            this.BackColor = backcolor;
+            
+        }
+
+        private void buttonColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorpickermode = "button";
+            colorDialog1.ShowDialog();
+            buttoncolor = colorDialog1.Color;
+            button1.BackColor = buttoncolor;
+            
+        }
+
+        private void textColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorpickermode = "text";
+            colorDialog1.ShowDialog();
+            textcolor = colorDialog1.Color;
+            listBox1.ForeColor = textcolor;
+            listBox2.ForeColor = textcolor;
+        }
+
+
+      
+    }
+}
